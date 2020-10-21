3017dd0b2bf1992de6b1e258f9eaed738f5341c1
Anders Ramsay <819213+andersr@users.noreply.github.com>
2020-10-21 19:11:21 -0400

    Add persistent input

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [value, setValue] = useState('')
+
+  useEffect(() => {
+    const str = localStorage.getItem('TEST_VALUE');
+    if (str) {
+      setValue(str);
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('TEST_VALUE', value);
+  }, [value])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input placeholder="persistent input" value={value} onChange={(e) => setValue(e.target.value)} />
     </div>
   );
 }
